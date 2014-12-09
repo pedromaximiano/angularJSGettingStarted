78ba1c30cd9e348c48c6026dfd4e608b889da7dd
pedromaximiano <pedro.maximiano@gmail.com>
2014-12-09 18:00:57 +0000

    Adds routing and views.

--- app.js
+++ app.js
@@ -2,61 +2,17 @@
   "use strict";
 
   // register angular module
-  var app = angular.module('HelloApp', []);
+  var app = angular.module('HelloApp', ['ngRoute']);
 
-  // the HelloController
-  var HelloController = function ($interval, $log, $anchorScroll, $location, github) {
-    var vm = this;
-
-    vm.error = false;
-    vm.message = "GitHub Projects";
-    vm.username = '';
-    vm.countdown = 5;
-    vm.countdownActive = null;
-
-    function onError(response) {
-      vm.error = true;
-      vm.reason = response.data.message || "Error getting data";
-      $log.error("Error: " + (response.data.message || "Error getting data"));
-    }
-
-    function onReposSuccess(response) {
-      vm.repos = response;
-      vm.error = false;
-    }
-
-    function onUserSuccess(response) {
-      vm.user = response;
-      vm.error = false;
-
-      github.getRepos(vm.username).then(onReposSuccess, onError);
-      $location.hash("userDetails");
-      $anchorScroll();
-    }
-
-    function decrementCountdown() {
-      vm.countdown -= 1;
-      if (vm.countdown < 1) {
-        vm.search();
-      }
-    }
-
-    vm.search = function () {
-      $log.info("Searching for " + vm.username);
-      github.getUser(vm.username).then(onUserSuccess, onError);
-      if (vm.countdownActive) {
-        $interval.cancel(vm.countdownActive);
-        vm.countdownActive = null;
-      }
-    };
-
-    vm.startCountdown = function () {
-      vm.countdownActive = $interval(decrementCountdown, 1000, vm.countdown);
-    };
-
-    vm.startCountdown();
-  };
-
-  // register the HelloController with angular
-  app.controller('HelloCtrl', ['$interval', '$log', '$anchorScroll', '$location', 'github', HelloController]);
+  app.config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'main.html',
+        controller: 'HelloCtrl',
+        controllerAs: 'hello'
+      })
+      .otherwise({
+        redirectTo: '/main.html'
+      });
+  });
 }());
